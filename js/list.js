2dc5b4464adbcf8ddb1ba4466d18916fb5d94e1b
SpaceQuacker <51411143+Bombardier-C-Kram@users.noreply.github.com>
2020-07-14 19:50:04 -0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,1 +1,13 @@
+window.addEventListener("DOMContentLoaded", function() {
+    var loginInput = document.getElementById("login--token");
+    var loginButton = document.getElementById("list--button");
 
+    loginButton.addEventListener("click", function() {
+        loginButton.setAttribute("disabled", "disabled");
+
+        authorizationHeader = btoa(loginInput.value);
+        sendList(function(status, response, xhr) {
+            alert(response);
+        });
+    });
+});
