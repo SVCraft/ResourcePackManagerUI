fa493482b251276d4f210c02e344cd907dbce2ac
SpaceQuacker <51411143+Bombardier-C-Kram@users.noreply.github.com>
2020-07-14 19:55:27 -0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -3,7 +3,6 @@ window.addEventListener("DOMContentLoaded", function() {
     var loginButton = document.getElementById("list--button");
 
     loginButton.addEventListener("click", function() {
-        loginButton.setAttribute("disabled", "disabled");
 
         authorizationHeader = btoa(loginInput.value);
         sendList(function(status, response, xhr) {
