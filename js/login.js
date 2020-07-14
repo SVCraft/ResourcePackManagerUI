8200dc55c3eec62b1e0327273ce73e6f513e4913
SpaceQuacker <51411143+Bombardier-C-Kram@users.noreply.github.com>
2020-07-14 16:34:01 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ window.addEventListener("DOMContentLoaded", function() {
         authorizationHeader = btoa(loginInput.value);
         sendPing(function(status, response, xhr) {
             if (response === "Pong! Your API key works.") {
-             document.getElementById("login").innerHTML = "<div id="panel--window"></div>";
+             document.getElementById("login").HTML = "<div id="panel--window"></div>";
             } else {
             alert("Your API key was incorrect, Please reload the page and try again!")
             }
