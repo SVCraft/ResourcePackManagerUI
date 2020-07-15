52434f241541594f89707ed620596a4388b10877
SpaceQuacker <51411143+Bombardier-C-Kram@users.noreply.github.com>
2020-07-15 09:36:45 -0300

    Make the button not disabled

--- js/list.js
+++ js/list.js
@@ -2,9 +2,11 @@ window.addEventListener("DOMContentLoaded", function() {
     var listButton = document.getElementById("list--button");
 
     listButton.addEventListener("click", function() {
+        listButton.setAttribute("disabled", "disabled");
 
         sendList(null, function(status, response, xhr) {
             alert(response);
+            listButton.removeAttribute("disabled");
         });
     });
 });
